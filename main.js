e00a182e1f483990450d664d234a0795a4a9c8b3
Bluhotsauce <bluhotsauce@gmail.com>
2021-01-28 21:05:37 -0500

    added 3 images

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
         let data = JSON.parse(this.responseText);
-        document.getElementById('dog image').src = data.message;
+        document.getElementById('dog image').src = data.message[0];
+        document.getElementById('dog image1').src = data.message[1];
+        document.getElementById('dog image2').src = data.message[2];
         console.log(data.message);
     }
 };
 
 function sendAJAX() {
-    xhr.open ('GET', `https://dog.ceo/api/breeds/image/random`);
+    xhr.open ('GET', `https://dog.ceo/api/breeds/image/random/3`);
     xhr.send();
 
 }
