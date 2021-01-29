84781579f684af57d85979ca323e7cb3494f50b4
Talyr Williams <talyrwilliams@Talyrs-MacBook-Air.local>
2021-01-28 21:32:55 -0500

    final

--- project.js
+++ project.js
@@ -2,8 +2,11 @@ let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
         let data = JSON.parse(this.responseText);
-        document.getElementById('dogimg').src = data.message;
+        document.getElementById('dogimg').src = data.message[0];
         console.log(data.message);
+
+        document.getElementById('dogimg').src = data.message[1];
+        document.getElementById('dogimg').src = data.message[2];
     }
 };
  function sendAJAX() {
@@ -13,6 +16,3 @@ xhr.onreadystatechange = function() {
     // let dogImg = new Image();
     // dogImg.src = "https://images.dog.ceo/breeds/husky/n02110185_11396.jpg";
 // }
-xhr.open('GET', `https://dog.ceo/api/breeds/image/random`);
-xhr.send();
-//document.getElementById('load').style.display = "none";
