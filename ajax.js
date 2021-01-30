d52f738b1293dbaf6e839c4b454643207814b4cd
Denae716 <denaebrown73@yahoo.com>
2021-01-29 19:29:32 -0500

    works for only the 1st time fetch is clicked

--- ajax.js
+++ ajax.js
@@ -1,11 +1,21 @@
 
 
 
-let xhr = new XMLHttpRequest();
+
+let xhr = new XMLHttpRequest
 xhr.onreadystatechange = function (){
-    if(this.readyState === 4 && this.status === 200){
-        console.log(xhr.responseText)
+if (this.readyState === 4 && this.status === 200){
+
+    let data =JSON.parse(this.response)
+    console.log(data)
+    document.getElementById("image").src = data.message
+    
+    
     }
 }
+
 xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+let fetch = function(){
 xhr.send();
+
+}
